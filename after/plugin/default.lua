14ee85942606e55b737474645335a5e739bcce73
Simon Proper <simon.proper@valtech.com>
2023-02-04 17:54:26 +0100

    fix: null-ls disable lsp native formater

--- after/plugin/default.lua
+++ after/plugin/default.lua
@@ -169,7 +169,12 @@ prequire('null-ls', function(null_ls)
           group = augroup,
           buffer = bufnr,
           callback = function()
-            vim.lsp.buf.format({ bufnr = bufnr })
+            vim.lsp.buf.format({
+              bufnr = bufnr,
+              filter = function(client)
+                return client.name == "null-ls"
+              end
+            })
             -- vim.lsp.buf.formatting_sync()
           end,
         })
