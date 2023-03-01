7c4bd774085af46c5699a99807515dd4ebf1edac
Simon Proper <simon.proper@outlook.com>
2023-03-01 21:33:39 +0100

    fix: cmp tailwind fix

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -9,6 +9,13 @@ if not snip_status_ok then
     return
 end
 
+-- -- cmp tailwind laggy fix
+-- local ok, nvim_cmp = pcall(require, 'nvim-cmp')
+--
+-- if not ok then
+--     return
+-- end
+
 cmp.setup {
     snippet = {
         expand = function(args)
@@ -16,7 +23,7 @@ cmp.setup {
         end
     },
     mapping = cmp.mapping.preset.insert {
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs( -4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<CR>'] = cmp.mapping.confirm {
@@ -31,58 +38,61 @@ cmp.setup {
             else
                 fallback()
             end
-        end, {'i', 's'}),
+        end, { 'i', 's' }),
         ['<S-Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
+            elseif luasnip.jumpable( -1) then
+                luasnip.jump( -1)
             else
                 fallback()
             end
-        end, {'i', 's'})
+        end, { 'i', 's' })
     },
-    sources = {{
+    sources = { {
         name = 'nvim_lsp'
     }, {
         name = 'luasnip'
-    }}
+    } },
+    window = {
+        documentation = {
+            border = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' },
+        },
+    },
+    experimental = {
+        ghost_text = true,
+        native_menu = false,
+    },
 }
 
--- cmp tailwind laggy fix
-local ok, nvim_cmp = pcall(require, 'nvim-cmp')
 
-if not ok then
-    return
-end
-
-nvim_cmp.setup({
-    sources = {{
-        name = 'nvim_lsp',
-        keyword_length = 6,
-        group_index = 1,
-        max_item_count = 30
-    }, {
-        name = 'path'
-    }, {
-        name = 'buffer'
-    }}
-    --[[ formatting = {
-      format = {
-        function(entry, vim_item)
-          if entry.completion_item.detail ~= nil and entry.completion_item.detail ~= "" then
-            vim_item.menu = entry.completion_item.detail
-          else
-            vim_item.menu = ({
-                  nvim_lsp = "[LSP]",
-                  luasnip = "[Snippet]",
-                  buffer = "[Buffer]",
-                  path = "[Path]",
-                })[entry.source.name]
-            return vim_item
-          end
-        end
-      },
-    }, ]]
-
-})
+-- nvim_cmp.setup({
+--     sources = {{
+--         name = 'nvim_lsp',
+--         keyword_length = 6,
+--         group_index = 1,
+--         max_item_count = 30
+--     }, {
+--         name = 'path'
+--     }, {
+--         name = 'buffer'
+--     }}
+--     --[[ formatting = {
+--       format = {
+--         function(entry, vim_item)
+--           if entry.completion_item.detail ~= nil and entry.completion_item.detail ~= "" then
+--             vim_item.menu = entry.completion_item.detail
+--           else
+--             vim_item.menu = ({
+--                   nvim_lsp = "[LSP]",
+--                   luasnip = "[Snippet]",
+--                   buffer = "[Buffer]",
+--                   path = "[Path]",
+--                 })[entry.source.name]
+--             return vim_item
+--           end
+--         end
+--       },
+--     }, ]]
+--
+-- })
