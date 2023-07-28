bb8a72748c135beb17d34b7ecdc378149babbaf1
Simon Proper <simon.proper@outlook.com>
2023-07-28 13:03:42 +0200

    fix(cmp): tailwind performance

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -9,13 +9,6 @@ if not snip_status_ok then
     return
 end
 
--- -- cmp tailwind laggy fix
--- local ok, nvim_cmp = pcall(require, 'nvim-cmp')
---
--- if not ok then
---     return
--- end
-
 cmp.setup {
     snippet = {
         expand = function(args)
@@ -23,7 +16,7 @@ cmp.setup {
         end
     },
     mapping = cmp.mapping.preset.insert {
-        ['<C-d>'] = cmp.mapping.scroll_docs( -4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<CR>'] = cmp.mapping.confirm {
@@ -42,21 +35,27 @@ cmp.setup {
         ['<S-Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_prev_item()
-            elseif luasnip.jumpable( -1) then
-                luasnip.jump( -1)
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
             else
                 fallback()
             end
         end, { 'i', 's' })
     },
     sources = { {
-        name = 'nvim_lsp'
+        name = 'nvim_lsp',
+        keyword_length = 2,
+        group_index = 1,
+        max_item_count = 30
     }, {
         name = 'luasnip'
     } },
     window = {
-        completion = cmp.config.window.bordered({ border = "rounded", winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None" }),
-        documentation = cmp.config.window.bordered({ border = "rounded", winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None" }), },
+        completion = cmp.config.window.bordered({ border = "rounded",
+            winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None" }),
+        documentation = cmp.config.window.bordered({ border = "rounded",
+            winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None" }),
+    },
     experimental = {
         ghost_text = true,
         native_menu = false,
