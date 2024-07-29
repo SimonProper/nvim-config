b4edc46e0aa1b012d0fa56347bc4ef5ef9288ebd
Simon Proper <simon.proper@outlook.com>
2024-07-29 22:21:39 +0200

    fix: nvim-tree default mappings in setup

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -4,27 +4,32 @@ if not ok then
     return
 end
 
-local function on_attach(bufnr)
-    local api = require('nvim-tree.api')
-
+local function custom_on_attach(bufnr)
+    local api = require "nvim-tree.api"
 
     local function opts(desc)
-        return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
     end
+
+    -- default mappings
+    api.config.mappings.default_on_attach(bufnr)
+
+    -- custom mappings
     vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close level'))
     vim.keymap.set('n', 'l', api.node.open.edit, opts('Edit file'))
+    vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
 end
 
 
 nvim_tree.setup({
+    on_attach = custom_on_attach,
     hijack_cursor = true,
-    on_attach = on_attach,
-    view= {
+    view = {
         adaptive_size = true
     }
 })
 
-local function open_nvim_tree(data)
+--[[ local function open_nvim_tree(data)
     -- buffer is a directory
   local directory = vim.fn.isdirectory(data.file) == 1
 
@@ -40,4 +45,4 @@ local function open_nvim_tree(data)
 end
 
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
-
+]]
