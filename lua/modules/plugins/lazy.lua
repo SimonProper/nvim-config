a4f664c8fd38b9bb9d332a805e5c59ded5a88063
Simon Proper <62900484+SimonProper@users.noreply.github.com>
2024-09-25 22:06:34 +0200

    refactor(lazygit): use telescope plugin

--- lua/modules/plugins/lazy.lua
+++ lua/modules/plugins/lazy.lua
@@ -204,16 +204,12 @@ return require('lazy').setup({
     -- LazyGit
     {
         "kdheepak/lazygit.nvim",
-        cmd = {
-            "LazyGit",
-            "LazyGitConfig",
-            "LazyGitCurrentFile",
-            "LazyGitFilter",
-            "LazyGitFilterCurrentFile",
-        },
-        -- optional for floating window border decoration
         dependencies = {
-            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim",
+            "nvim-lua/plenary.nvim"
         },
+        config = function()
+            require("telescope").load_extension("lazygit")
+        end,
     },
 })
