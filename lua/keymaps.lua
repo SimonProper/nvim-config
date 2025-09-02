55cb82d62d85097f7f826835dcfbb253ac377cad
Simon Proper <62900484+SimonProper@users.noreply.github.com>
2025-09-02 14:25:29 +0200

    feat(keymaps): visual copy to clipboard

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -45,4 +45,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.keymap.set('v', '<C-c>', '"+y', { desc = 'Move focus to the upper window', silent = true })
+
 -- vim: ts=2 sts=2 sw=2 et
