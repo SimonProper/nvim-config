6e935dca153896c0c474328e59367672c6319e63
Simon Proper <62900484+SimonProper@users.noreply.github.com>
2025-12-20 10:58:45 +0100

    refactor(lsp): move server config to nvim 11+ setup

---
+++ after/lsp/ts_ls.lua
@@ -0,0 +1,11 @@
+-- This file is automatically loaded by Neovim 0.11
+return {
+  -- Your specific settings go here
+  init_options = {
+    preferences = {
+      importModuleSpecifierPreference = 'project-relative',
+    },
+  },
+  -- You can also add filetypes or root_markers if needed
+  -- filetypes = { "typescript", "typescriptreact" },
+}
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -36,31 +36,6 @@ return {
       'tpope/vim-sleuth',
     },
     config = function()
-      -- Brief aside: **What is LSP?**
-      --
-      -- LSP is an initialism you've probably heard, but might not understand what it is.
-      --
-      -- LSP stands for Language Server Protocol. It's a protocol that helps editors
-      -- and language tooling communicate in a standardized fashion.
-      --
-      -- In general, you have a "server" which is some tool built to understand a particular
-      -- language (such as `gopls`, `lua_ls`, `rust_analyzer`, etc.). These Language Servers
-      -- (sometimes called LSP servers, but that's kind of like ATM Machine) are standalone
-      -- processes that communicate with some "client" - in this case, Neovim!
-      --
-      -- LSP provides Neovim with features like:
-      --  - Go to definition
-      --  - Find references
-      --  - Autocompletion
-      --  - Symbol Search
-      --  - and more!
-      --
-      -- Thus, Language Servers are external tools that must be installed separately from
-      -- Neovim. This is where `mason` and related plugins come into play.
-      --
-      -- If you're wondering about lsp vs treesitter, you can check out the wonderfully
-      -- and elegantly composed help section, `:help lsp-vs-treesitter`
-
       --  This function gets run when an LSP attaches to a particular buffer.
       --    That is to say, every time a new file is opened that is associated with
       --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
@@ -119,35 +94,6 @@ return {
           vim.api.nvim_buf_create_user_command(event.buf, 'LspDiagLine', vim.diagnostic.open_float, {})
           vim.keymap.set('n', 'gf', ':LspDiagLine<CR>', { silent = true })
 
-          -- The following two autocommands are used to highlight references of the
-          -- word under your cursor when your cursor rests there for a little while.
-          --    See `:help CursorHold` for information about when this is executed
-          --
-          -- When you move your cursor, the highlights will be cleared (the second autocommand).
-          -- local client = vim.lsp.get_client_by_id(event.data.client_id)
-          -- if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-          --   local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
-          --   vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-          --     buffer = event.buf,
-          --     group = highlight_augroup,
-          --     callback = vim.lsp.buf.document_highlight,
-          --   })
-          --
-          --   vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-          --     buffer = event.buf,
-          --     group = highlight_augroup,
-          --     callback = vim.lsp.buf.clear_references,
-          --   })
-          --
-          --   vim.api.nvim_create_autocmd('LspDetach', {
-          --     group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
-          --     callback = function(event2)
-          --       vim.lsp.buf.clear_references()
-          --       vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
-          --     end,
-          --   })
-          -- end
-
           -- The following code creates a keymap to toggle inlay hints in your
           -- code, if the language server you are using supports them
           --
@@ -197,7 +143,25 @@ return {
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+
+        ---@type vim.lsp.Config
+        ts_ls = {
+          settings = {
+            typescript = {
+              preferences = {
+                importModuleSpecifierPreference = 'project-relative',
+              },
+            },
+            javascript = {
+              preferences = {
+                importModuleSpecifierPreference = 'project-relative',
+              },
+            },
+          },
+          init_options = {
+            hostInfo = 'janne',
+          },
+        },
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
@@ -236,12 +200,14 @@ return {
       require('mason-lspconfig').setup {
         handlers = {
           function(server_name)
-            local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
+            local server_opts = servers[server_name] or {}
+
+            -- 1. Use the new 0.11 API to define the config
+            -- This merges your custom 'server_opts' with the defaults from nvim-lspconfig
+            vim.lsp.config(server_name, server_opts)
+
+            -- 2. Enable the server (this replaces .setup())
+            vim.lsp.enable(server_name)
           end,
         },
       }
